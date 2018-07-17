fee2f6af20625ce4fd9fbbfeb71c042b668c65ce
wuyijun <tanke.wyj@alibaba-inc.com>
2018-07-17 22:06:55 +0800

    Initial check in

---
+++ src/java/_05_coroutines/kt/InfiniteStreams.kt
@@ -0,0 +1,21 @@
+package _05_coroutines.kt
+
+import kotlin.coroutines.experimental.buildSequence
+
+fun main(args: Array<String>) {
+    val seq = buildSequence {
+        var a = 1
+        var b = 1
+
+        while (true) {
+            yield(a)
+            val tmp = a
+            a = b
+            b += tmp
+        }
+    }
+
+    println(
+        seq.take(20).toList()
+    )
+}
